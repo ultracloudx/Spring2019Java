be7f2e2b08f73f1a7f4f7abaf5820d3bed3845c6
ultracloudx_home <alexander.ta2007@gmail.com>
2019-03-14 00:45:42 -0500

    update 3/14/2019 12am

--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.lang.String;
 
 public class ShoppingCart {
 
@@ -8,7 +9,7 @@ public class ShoppingCart {
 
     public ShoppingCart(String customerName, String currentDate) {
         this.customerName = customerName;
-        this.currentDate = currentDate;
+        this.currentDate = "January 1, 2016";
         this.cartItems = new ArrayList<ItemToPurchase>();
     }
 
@@ -21,20 +22,38 @@ public class ShoppingCart {
     }
 
     public void addItem(ItemToPurchase item) {
-
+        cartItems.add(0, item);
     }
 
     public void removeItem(String item) {
+        int i;
 
+        for (i = 0; i < cartItems.size(); i++) {
+            if (cartItems.get(i).equals(item)) {
+                cartItems.remove(i);
+                break;
+            } else {
+                System.out.println("Item not found in car. Nothing removed");
+            }
+        }
     }
 
     public void modifyItem(ItemToPurchase item) {
+        int i;
+
+        for (i = 0; i < cartItems.size(); i++) {
+            if (cartItems.get(i).equals(item)) {
+
+            } else {
+                System.out.println("Item not found in cart. Nothing modified.");
+            }
+        }
 
     }
 
     public int getNumItemsInCart() {
         int total = 0;
-
+        total = cartItems.size();
 
         return total;
     }
@@ -47,6 +66,7 @@ public class ShoppingCart {
     }
 
     public void printTotal() {
+        //System.out.println("SHOPPING CART IS EMPTY);
 
 
     }
--- src/ShoppingCartManager.java
+++ src/ShoppingCartManager.java
@@ -7,10 +7,28 @@ public class ShoppingCartManager {
     public static char printMenu(ShoppingCart cart) {
         char choice = 'z';
 
+        System.out.println("" +
+                "MENU\n" +
+                "a - Add item to cart\n" +
+                "d - Remove item from cart\n" +
+                "c - Change item quantity\n" +
+                "i - Output items' descriptions\n" +
+                "o - Output shopping cart\n" +
+                "q - Quit\n\n" +
+                "Choose an option:");
+        //choice = scanner.nextLine().charAt();
+
         return choice;
     }
 
     public static void main(String arg[]) {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        String inputDate;
+        String inputName;
 
+        System.out.println("Enter Customer's Name:");
+
+
+        //shoppingCart.getDate();
     }
 }
