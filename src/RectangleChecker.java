0c569d46a9b73ba7889409c71799367b83579458
ultracloudx <alexander.ta2007@gmail.com>
2019-04-04 11:28:44 -0500

    first commit

--- src/RectangleChecker.java
+++ src/RectangleChecker.java
@@ -6,14 +6,14 @@ public class RectangleChecker {
         Scanner scanner = new Scanner(System.in);
         int pointX, pointY, height, width;
 
-        System.out.println("Enter x coordinate:");
+        System.out.println("Enter x coordinate: ");
         pointX = scanner.nextInt();
-        System.out.println("Enter y coordinate:");
+        System.out.println("Enter y coordinate: ");
         pointY = scanner.nextInt();
 
-        System.out.println("Enter width:");
+        System.out.println("Enter width: ");
         width = scanner.nextInt();
-        System.out.println("Enter height:");
+        System.out.println("Enter height: ");
         height = scanner.nextInt();
 
         BetterRectangle myRect = new BetterRectangle(pointX, pointX, height, width);
